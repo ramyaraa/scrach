84e8f7ff10ada450f090cdb878f98f3d258952f9
ramyar mohammed <adammhamad12300@gmail.com>
2024-04-20 19:50:44 +0300

    good

---
+++ java/Main.java
@@ -0,0 +1,12 @@
+public class Main {
+    public static void main(String[] args) {
+        int i= 0;
+        while(i<5){
+            System.out.println("hi");
+            i++;
+        }
+
+
+    }
+}
+
