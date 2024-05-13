7f9257f21d828abec41b34d2a547e3f8eb3fe7e2
ramyar mohammed <adammhamad12300@gmail.com>
2024-05-14 01:29:04 +0300

    add some java code

---
+++ java/star.java
@@ -0,0 +1,119 @@
+public class star {
+ public static void main(String[] args) {
+    int n =5;
+
+    //////// segosha sar chap
+    // for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i+j>=4){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+    //   for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(j>i){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+
+    
+    /////////// segosha sar rast
+    // for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i>=j){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+    //   for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i+j>=n-1){
+        
+    //           System.out.print("  ");
+    //           //   continue;
+    //         }
+    //         else{
+    //             System.out.print("* ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+
+        ////////// ma3en rast chap
+    // for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i>=j){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+    //   for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(j>i){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+
+    ////////// ma3en chap rast
+    // for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i+j>=n-1){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //           System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+    //   for (int i=0 ; i<n ;i++){
+    //     for(int j=0;j<n; j++){
+    //       if(i+j<n-1){
+        
+    //           System.out.print("* ");
+    //           //   continue;
+    //         }
+    //         else{
+    //             System.out.print("  ");
+    //       }
+    //       }
+    //       System.out.println(" ");
+    //   }
+
+ }
+}
